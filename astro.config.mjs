0b138621938ea1bc8682e83d64c2bd821ce4af85
alexander.mospan <alexander.mospan@smartengines.ru>
2026-05-19 23:09:21 +0300

    hero block in progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import { fileURLToPath, URL } from "node:url"
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    build: {
+      minify: false,
+      cssMinify: false,
+      cssCodeSplit: true,
+      assetsInlineLimit: 0, // отключает инлайн маленьких файлов
+      rollupOptions: {
+        output: {
+          assetFileNames: "assets/[name][extname]",
+          chunkFileNames: "assets/[name].js",
+          entryFileNames: "assets/[name].js",
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  },
+})
